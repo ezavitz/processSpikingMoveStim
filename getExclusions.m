b10f497ed1cb3449088c7bc095d9b2a7d995593a
Elizabeth Zavitz <elizabeth.zavitz@monash.edu>
2022-05-09 14:40:16 +1000

    add OK.byTime exclusion information

--- getExclusions.m
+++ getExclusions.m
@@ -1,7 +1,6 @@
 % getExclusionCriteria
 function [OK] = getExclusions(tcs)
 
-
 % figure out some data-related parameters
 types = fieldnames(tcs.Move);                   % what types
 [nAreas, nDirs] = size(tcs.Move.(types{1}));    % how many areas + dirs
@@ -11,38 +10,38 @@ tmp = cellfun(@(x) size(x, 1), ...
                 tcs.Move.(types{1}));
 nUnits = tmp(:, 1);                             % how many units per area                    
 
-% preallocate
-for ia = 1:nAreas
-   OK.isVisual{ia} = zeros(nTypes, nUnits(ia));   
-   OK.anyVisual{ia} = zeros(1, nUnits(ia));   
-   OK.DSI{ia} = zeros(nTypes, nUnits(ia));
-end
-
-
 % Must respond more to move than blank throughout experiment
 %   in at least one of the 12 directions
 
 % parameters 
 pThresh = 0.05;
-nBins = 4;
-
+trialStep = 10; %test 10 trials at a time for visual responsiveness (move > blank)
 
 for ia = 1:nAreas   
 for it = 1:nTypes
-    overThresh = zeros(nUnits(ia), nDirs);
+    p = nan(nUnits(ia), nBins, nDirs);
 for id = 1:nDirs
     tmpMV = tcs.Move.(types{it}){ia,id};
     tmpBK = tcs.Blank.(types{it}){ia,id};
     
     nTrials = size(tmpBK, 2);
-    trialStep = nTrials/nBins;
+    nBins = ceil(nTrials/trialStep);
     binStarts = 1:trialStep:nTrials;
+    
+    %preallocate
+    if it == 1 && id == 1
+        OK.byTime{ia} = zeros(nTypes, nUnits(ia), nTrials);
+        OK.isVisual{ia} = zeros(nTypes, nUnits(ia));   
+        OK.anyVisual{ia} = zeros(1, nUnits(ia));   
+        OK.DSI{ia} = zeros(nTypes, nUnits(ia));
+    end
+    
     for iu = 1:nUnits(ia)
-        p = zeros(1, nBins);
         for ib = 1:nBins
             trialRange = binStarts(ib):binStarts(ib)+trialStep-1;
-            p(ib) = kruskalwallis([tmpBK(iu,trialRange)' ...
-                                   tmpMV(iu,trialRange)'], [], 'off');
+            trialRange = trialRange(trialRange <= length(tmpBK)); %last bin may have fewer trials
+            p(iu, ib, id) = ranksum(tmpBK(iu,trialRange), ...
+                                    tmpMV(iu,trialRange), 'tail', 'left');
         end
         % if all bins are significantly modulated
         if prod(p < pThresh) ~= 0 
@@ -50,10 +49,13 @@ for id = 1:nDirs
         end
     end
 end
+    h = p < pThresh; % check p-value against alpha
+    hAnyDir = sum(h, 3) > 0;
+    OK.byTime{ia}(it, :, :) = repelem(hAnyDir, 1, trialStep);
 end
     OK.anyVisual{ia} = sum(OK.isVisual{ia}) > 0;
 end
-    
+   
 % DSI Calculation
 
 for ia = 1:nAreas  
