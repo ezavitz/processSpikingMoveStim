d779449e7e8421cfebd8bc6c51451bfaca4be57a
Elizabeth Zavitz <elizabeth.zavitz@monash.edu>
2023-02-03 15:25:38 +1100

    test spike onsets

--- debugTrialOnsets.m
+++ debugTrialOnsets.m
@@ -1,4 +1,4 @@
-rootDir = '~/Documents/code/corrStructure_V1MT/data/CJ223/002';
+rootDir = '~/om94/data/CJ223/003';
 
 tmp = split(rootDir, filesep); % get the animal mame
 expression = sprintf('%s\\.motionStim\\.[0-9]{6}\\.mat', tmp{end-1});
@@ -6,27 +6,39 @@ expression = sprintf('%s\\.motionStim\\.[0-9]{6}\\.mat', tmp{end-1});
 % get a list of the neurostim files that are part of this penetration
 fileList = getFileListFromDirs([rootDir filesep], expression);
 cfg = 'marmodata.cfgs.acute.H64FlexiH64FlexiIntan';
-%%
-d = marmodata.mdbase([rootDir filesep fileList{1}],'loadArgs', ...
+%
+for fileID = 1:4
+    fprintf('File %i \n', fileID);
+    d{fileID} = marmodata.mdbase([rootDir filesep fileList{fileID}],'loadArgs', ...
     {'loadEye',false,'spikes',true,'source','ghetto', ...
     'reload',true, 'channels', 66, 'useCAR',false}); %% need to remove channels argument to process all
 
-%%
-load([rootDir filesep fileList{1}], 'c'); 
-oris = get(c.nWave.prms.orientation, 'atTrialTime', Inf)';
-
-%%
-sc = zeros(1, length(d.spikes.spk)); 
-for iTrial = 1:length(d.spikes.spk)
-    sc(iTrial) = sum(d.spikes.spk{iTrial}(d.spikes.spk{iTrial} < 0.55));
-end
-%%
-oriL = unique(oris); 
-subSC = sc(2:end);
-tc = zeros(1, length(oriL));
-for oi = 1:length(oriL)
-    theseTrials = oris == oriL(oi);
-    tc(oi) = mean(subSC(theseTrials));
+    load([rootDir filesep fileList{fileID}], 'c'); 
+    oris{fileID} = get(c.nWave.prms.orientation, 'atTrialTime', Inf)';
+    
+    sc{fileID} = zeros(1, length(d{fileID}.spikes.spk)); 
+    for iTrial = 1:length(d{fileID}.spikes.spk)
+        sc{fileID}(iTrial) = sum(d{fileID}.spikes.spk{iTrial}(d{fileID}.spikes.spk{iTrial} < 0.55));
+    end
 end
 
-plot(tc);
+
+
+
+%%
+ 
+for iFile = 1:length(sc)
+    oriL = unique(oris{iFile});
+        if length(sc{iFile}) > length(oris{iFile})
+            subSC = sc{iFile}(2:end);
+        else
+           subSC = sc{iFile};
+        end
+    tc = zeros(1, length(oriL));
+    for oi = 1:length(oriL)
+        theseTrials = oris{iFile} == oriL(oi);
+        tc(oi) = mean(subSC(theseTrials));
+    end
+
+    plot(tc); hold on;
+end
