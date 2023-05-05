b77109037197638911bdc8c08e65e016f48361f3
Elizabeth Zavitz <elizabeth.zavitz@monash.edu>
2023-05-05 15:37:15 +1000

    split off spike extraction for non-kilosort mdb files

---
+++ RUNAnalysis_mdbExtract.m
@@ -0,0 +1,40 @@
+if ismac 
+    cd('/Users/ezavitz/Documents/code/processSpikingMoveStim');
+else
+    cd('~/code/processSpikingMoveStim');
+end
+
+configureBatch_mdb; 
+
+fPre = ''; % use this to prefix other versions of preprocessed data
+
+force = 1;
+
+prefixes = {'V1','MT'};
+
+chanOrder{1} = 1:96; %to do -- make this a reshaped Utah channel 10x10 map
+chanOrder{2} = [13 23 5 25 6 24 14 27 7 26 4 28 8 20 3 29 9 19 2 31 10 30 1 32 11 22 12 21 15 18 16 17];
+
+% file is a cell array of penetrations with fields animalName, ID, Utah mapFile, etc.      
+param.allTypes = {'Dots', 'SineWave', 'Square', 'PSsquare'}; 
+param.windows = {'Move', 'Blank'}; 
+param.window{1} = [50 550];
+param.window{2} = [650 1000];
+param.nTrialsSlowWin = 0; % number of trials over which to calculate the slow fluctuation of the mean
+                           % set this to <= 0 to z-score over all trials at once.
+% for SDFs
+param.sWin = 1000; %SDF window size (ms)
+param.sBin = 50;   %SDF boxcar size (ms)
+
+tmp = split(allFiles{pen}, filesep);
+
+saveName = sprintf('%s%s%scombinedData_%i.mat', rootDir, filesep, fPre, whichCh);
+
+if ~exist(saveName, 'file') || force
+    [sTrain, onsetInds, StimFile, chanOrder] = combineDataMarmolab(rootDir, param.allTypes, whichCh);
+
+    fprintf('\n Saving %s \n', saveName);
+    save(saveName, 'sTrain', 'onsetInds', 'StimFile', ...
+                   'param', 'chanOrder', '-v7.3');
+end
+
---
+++ configureBatch_mdb.m
@@ -0,0 +1,28 @@
+% the marmodata version of processing things - with channel-by-channel
+% spike extraction
+if getenv('SLURM_ARRAY_TASK_ID')
+    idStr = getenv('SLURM_ARRAY_TASK_ID'); % get task ID string 
+    fprintf('ID %s\n', idStr);
+	arrayTaskID = str2num(idStr); % conv string to number
+    
+    [chs, pens] = meshgrid(1:128, 1:4);
+    
+    whichCh = chs(arrayTaskID); 
+    pen     = pens(arrayTaskID);
+    
+    thisJob = getenv('$SLURM_JOB_NAME');
+else
+    whichCh = 32;
+    pen = 1;
+end
+
+allFiles = {'/CJ221/001', ...
+            '/CJ221/007', ...
+            '/CJ223/003', ...
+            '/CJ223/004'};
+        
+fprintf('Pen %i Ch %i \n', pen, whichCh); 
+
+configurePaths;
+rootDir = [thisPathRoot allFiles{pen}];          
+
