d6dbc59ce0e460631177308dff98615a3998db60
Elizabeth Zavitz <elizabeth.zavitz@monash.edu>
2023-05-24 11:53:50 +1000

    add sbatch files

--- configureBatch_mdb.m
+++ configureBatch_mdb.m
@@ -1,11 +1,15 @@
 % the marmodata version of processing things - with channel-by-channel
 % spike extraction
+allFiles = {'/CJ221/001', ...
+            '/CJ223/001', ...
+            '/CJ223/003'};
+
 if getenv('SLURM_ARRAY_TASK_ID')
     idStr = getenv('SLURM_ARRAY_TASK_ID'); % get task ID string 
     fprintf('ID %s\n', idStr);
 	arrayTaskID = str2num(idStr); % conv string to number
     
-    [chs, pens] = meshgrid(1:128, 1:4);
+    [chs, pens] = meshgrid(1:128, 1:length(allFiles));
     
     whichCh = chs(arrayTaskID); 
     pen     = pens(arrayTaskID);
@@ -16,9 +20,7 @@ else
     pen = 1;
 end
 
-allFiles = {'/CJ221/001', ...
-            '/CJ223/001', ...
-            '/CJ223/003'};
+
         
 fprintf('Pen %i Ch %i \n', pen, whichCh); 
 
