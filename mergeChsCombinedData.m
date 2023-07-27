004e475b4368faeeb03a8c0e64af5f091a959dcb
Elizabeth Zavitz <elizabeth.zavitz@monash.edu>
2023-07-27 12:10:26 +1000

    cope with spikes long after stimulus, discontinuous channels

--- mergeChsCombinedData.m
+++ mergeChsCombinedData.m
@@ -8,7 +8,9 @@ configurePaths;
 dataDir = [rootDir filesep]; %sprintf('%s%s%s', thisPathRoot, allFiles{pen}, filesep);
 fileNames = getFileListFromDirs(dataDir, 'combinedData_[0-9]+\.mat');
 
-nCh = length(fileNames); 
+nCh = length(fileNames);
+chNums = sort(cellfun(@(x) str2double(extractBefore(x(14:end),'.')), fileNames)); 
+
 areaBreak = 64; % what channel is the last channel in V1?
 nChTot = min(areaBreak, nCh);
 
@@ -18,25 +20,26 @@ chInd = 1;
 
 for iCh = 1:nCh
     % load the data
-    filename = sprintf('combinedData_%i.mat', iCh);
+    filename = sprintf('combinedData_%i.mat', chNums(iCh));
     fprintf('Loading %s \n', filename); 
     load([dataDir filename])
-
+    
     % initialise the data structures
     if iCh == 1 || iCh == areaBreak+1 %if this is the first V1 or MT channel
         for subfile = 1:length(sTrain) % data is broken across multiple recordings
             if iCh == 1
                 sTrainAll{subfile} = cell(1,2);
             end
+            maxSize(subfile) = max(onsetInds{subfile}{aInd}(1, :))+4500;
             sTrainAll{subfile}{aInd} = ... %initialise
-                zeros(nChTot, size(sTrain{subfile}{1}, 2)); 
+                zeros(nChTot, maxSize(subfile)); 
         end
     end
 
     % copy over the spiking data
     for subfile = 1:length(sTrain)
         sTrainAll{subfile}{aInd}(chInd, :) = ...
-            sTrain{subfile}{1};
+            sTrain{subfile}{1}(1:maxSize(subfile));
     end
 
     % increment counters
